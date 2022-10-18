e03c39cda4ad5a88f8e237753ad2e3d1664ac2ba
Davide-Milan <davidemilan27@gmail.com>
2022-10-19 00:34:07 +0200

    Create login form project

---
+++ Signup form/script.js
@@ -0,0 +1,13 @@
+const email = document.querySelector('#email');
+const pwd = document.querySelector('#password');
+const cpwd = document.querySelector('#cpassword');
+const phone = document.querySelector('#phone');
+const submit = document.querySelector('#form__submit');
+
+
+
+let checkPwd = (e) => {
+
+}
+
+cpwd.addEventListener('input', checkPwd);
