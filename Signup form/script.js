a5bb80b1d6a41abd4ff829a77a4dce75748911b6
Davide-Milan <davidemilan27@gmail.com>
2022-10-21 20:04:53 +0200

    Finish signup form project

--- Signup form/script.js
+++ Signup form/script.js
@@ -5,9 +5,16 @@ const phone = document.querySelector('#phone');
 const submit = document.querySelector('#form__submit');
 
 
-
 let checkPwd = (e) => {
-
+    if(pwd.value != cpwd.value){
+        e.preventDefault();
+        pwd.classList.add('error');
+        cpwd.classList.add('error');
+    }
+    else{
+        pwd.classList.remove('error');
+        cpwd.classList.remove('error');
+    }
 }
 
-cpwd.addEventListener('input', checkPwd);
+submit.addEventListener('click', checkPwd);
